cf296ea9e2c405fdfa1f18a2aaa93042a22bfe45
CassieJie <1597212316@qq.com>
2020-03-26 15:41:43 +0800

    v3.2

--- routes/index.js
+++ routes/index.js
@@ -9,36 +9,37 @@ router.use(bodyParser.json());
 var DB = require('../modules/db.js');
 
 
-router.post('/search',function(req,res){
-    // var Date = req.body.Date;
-    //
-    // // 2.连接数据库查询数据
-    // if (Date == "") {
-    //     DB.find('honor', {}, function (err, data) {
-    //         res.render('css', {
-    //             list: data
-    //         })
-    //     })
-    // }
-    // else {
-    //     var map = {"01":"Jan","02":"Feb","03":"Mar","04":"Apr","05":"May","06":"Jun","07":"Jul","08":"Aug","09":"Sep","10":"Oct","11":"Nov","12":"Dec"};
-    //     var l =map[Date.split('/')[1]];
-    //     Date =l+" "+Date.split('/')[0];
-    //
-    //     DB.find('honor', {
-    //         Date: Date,
-    //     }, function (err, data) {
-    //         if (data.length > 0) {
-    //             res.render('css', {
-    //                 list: data
-    //             })
-    //         } else {
-    //             res.send("There is no match for search date");
-    //         }
-    //     })
-    // }
+router.post('/searchByDate',function(req,res){
+    var Date = req.body.Date;
 
+    // 2.连接数据库查询数据
+    if (Date == "") {
+        DB.find('honor', {}, function (err, data) {
+            res.render('css', {
+                list: data
+            })
+        })
+    }
+    else {
+        var map = {"01":"Jan","02":"Feb","03":"Mar","04":"Apr","05":"May","06":"Jun","07":"Jul","08":"Aug","09":"Sep","10":"Oct","11":"Nov","12":"Dec"};
+        var l =map[Date.split('/')[1]];
+        Date =l+" "+Date.split('/')[0];
+
+        DB.find('honor', {
+            Date: Date,
+        }, function (err, data) {
+            if (data.length > 0) {
+                res.render('css', {
+                    list: data
+                })
+            } else {
+                res.send("There is no match for search date");
+            }
+        })
+    }
+});
     //ID
+router.post('/searchById',function(req,res){
     var id = req.body.searchId;
     if (id == "") {
         DB.find('honor', {}, function (err, data) {
@@ -63,6 +64,8 @@ router.post('/search',function(req,res){
     }
 
 });
+
+
 router.post('/pop_up_search',function(req,res) {
     var id = req.body.id;
     // 2.连接数据库查询数据
